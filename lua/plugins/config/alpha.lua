81a72955bd2fb6485f30bcbd5c2d56359bf4ef6e
Andrew Barlow <dandrewbarlow@gmail.com>
2025-03-11 14:47:32 -0500

    sort recent sessions in startup display

--- lua/plugins/config/alpha.lua
+++ lua/plugins/config/alpha.lua
@@ -30,18 +30,32 @@ return {
         local mini_sessions = require('mini.sessions')
         local session_list = mini_sessions.detected
 
-        -- loop through session list, and insert new buttons for them into sessions_section
-        local i = 0
-        for key, _ in pairs(session_list) do
+        -- sorter function, working with my custom array below and the MiniSessions.detected.modify_time property
+        local sorter = function(session_a, session_b)
+            return session_a[2].modify_time > session_b[2].modify_time
+        end
 
-            local button_map = "<leader>" .. tostring(i)
-            local button_text = "  " .. key
-            local cmd_string = "<cmd>lua MiniSessions.read(\"" .. tostring(key) .. "\")<CR>"
+        -- create a new list, holding the key, and value in their own array
+        local sorted_session_list = {}
+        for k, j in pairs(session_list) do
+            local el = {k, j}
+            table.insert(sorted_session_list, el)
+        end
+
+        -- sort the new list by most recently used
+        table.sort(sorted_session_list, sorter)
+
+        -- -- loop through session list, and insert new buttons for them into sessions_section
+
+        for index, value in ipairs(sorted_session_list) do
+
+            local button_map = "<leader>" .. tostring(index-1)
+            local button_text = "  " .. value[1]
+            local cmd_string = "<cmd>lua MiniSessions.read(\"" .. tostring(value[1]) .. "\")<CR>"
 
             local button = dashboard.button(button_map, button_text, cmd_string)
 
             table.insert(sessions_section.val, button)
-          i = i+1
         end
 
         local custom_dash = theta
