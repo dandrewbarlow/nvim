e8ebe4e2d0046aa73ba2162e753155862b541538
Andrew Barlow <dandrewbarlow@gmail.com>
2025-08-19 16:41:16 -0500

    Update Telescope
    0.1.5->0.1.8

--- lua/plugins/navigation.lua
+++ lua/plugins/navigation.lua
@@ -1,7 +1,7 @@
 
 return {
   { -- Telescope: unlock the power of fuzzy finding
-    'nvim-telescope/telescope.nvim', tag = '0.1.5',
+    'nvim-telescope/telescope.nvim', tag = '0.1.8',
     requires = require('plugins.config.telescope').requires,
     config = require('plugins.config.telescope').config,
   },
